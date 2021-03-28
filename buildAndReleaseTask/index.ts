253e8a70e9ca73d7dcba921b94eee39ccaea9178
imjoseangel <josea.munoz@gmail.com>
2021-03-28 14:23:09 +0200

    improving a little

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -1,12 +1,20 @@
 import tl = require('azure-pipelines-task-lib/task');
+import tr = require('azure-pipelines-task-lib/toolrunner');
 
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function run() {
     try {
-        const inputString: string | undefined = tl.getInput('flag', true);
-        const { stdout, stderr } = await exec('ansible -ilocahost, -m debug -a msg=' + inputString + ' localhost');
+        let inputString = tl.getInput('flag', true);
+        //const inputString: string | undefined = tl.getInput('flag', true);
+
+        console.log('========================== Starting Command Output ===========================');
+
+        let ansiblePath: string = tl.which('ansible', true);
+
+        const { stdout, stderr } = await exec(ansiblePath + ' -ilocahost, -m debug -a msg=' + inputString + ' localhost');
+
         console.log('stdout:', stdout.trim());
         console.log('stderr:', stderr.trim());
     } catch (err) {

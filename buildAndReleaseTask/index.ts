0851ef0e9eef592c7285f2c09a063f52b7d268a8
imjoseangel <josea.munoz@gmail.com>
2021-03-28 13:56:25 +0200

    Add ansible run command

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -6,9 +6,9 @@ const exec = util.promisify(require('child_process').exec);
 async function run() {
     try {
         const inputString: string | undefined = tl.getInput('flag', true);
-        const { stdout, stderr } = await exec('ls ', inputString);
-        console.log('stdout:', stdout);
-        console.log('stderr:', stderr);
+        const { stdout, stderr } = await exec('ansible -ilocahost, -m debug -a msg=' + inputString + ' localhost');
+        console.log('stdout:', stdout.trim());
+        console.log('stderr:', stderr.trim());
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     };
